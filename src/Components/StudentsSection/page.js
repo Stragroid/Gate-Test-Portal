e21b7ee09e4c655a1e79010ee4958ec29a1c6577
Hans Pattnaik <pattnaikhans@gmail.com>
2023-06-19 16:32:26 +0530

    Admin Page Update

--- src/Components/StudentsSection/page.js
+++ src/Components/StudentsSection/page.js
@@ -1,9 +1,122 @@
+import { useEffect, useState } from "react";
+import { db } from "../../firebaseConfig";
+import { collection, query, getDocs, updateDoc, doc } from "firebase/firestore";
 import "./style.css";
+import { CSVLink, CSVDownload } from "react-csv";
 
 export default function StudentsSection() {
+  const [students, setStudents] = useState([]);
+  const [csvData, setCsvData] = useState([
+    ["Sl. No.", "Username", "Email", "Attendance", "Marks"],
+  ]);
+
+  useEffect(() => {
+    getDocs(query(collection(db, "students"))).then((querySnapshot) => {
+      let tempStuds = [];
+      let tempCsvData = csvData;
+      querySnapshot.forEach((doc) => {
+        tempStuds.push(doc.data());
+      });
+      getDocs(query(collection(db, "test"))).then((querySnapshot) => {
+        let tempQuestions = [];
+        querySnapshot.forEach((doc) => {
+          tempQuestions = doc.data().questions;
+        });
+        tempQuestions = Object.values(tempQuestions);
+        for (let i = 1; i <= tempQuestions.length; i++) {
+          tempCsvData[0].push(`q${i}`);
+        }
+        tempStuds.forEach((student, index) => {
+          let marks = 0;
+          if (student.attended) {
+            let answers = Object.values(student.answers);
+            for (let i = 0; i < answers.length; i++) {
+              if (answers[i].answer === '') continue;
+              if (tempQuestions[i].questionType === "somcq") {
+                if (answers[i].answer === tempQuestions[i].a) {
+                  marks += tempQuestions[i].marksOnCorrect;
+                } else {
+                  marks += tempQuestions[i].marksOnIncorrect;
+                }
+              } else if (tempQuestions[i].questionType === "momcq") {
+                if (
+                  Array.from(answers[i].answer).sort().toString() ===
+                  Array.from(tempQuestions[i].a).sort().toString()
+                ) {
+                  marks += tempQuestions[i].marksOnCorrect;
+                } else {
+                  marks += tempQuestions[i].marksOnIncorrect;
+                }
+              } else {
+                if (
+                  parseFloat(answers[i].answer) ===
+                  parseFloat(tempQuestions[i].a)
+                ) {
+                  marks += tempQuestions[i].marksOnCorrect;
+                } else {
+                  marks += tempQuestions[i].marksOnIncorrect;
+                }
+              }
+            }
+          }
+          student.marks = marks;
+          //   console.log(student);
+          let tempCsvArray = [
+            index + 1,
+            student.username,
+            student.email,
+            student.attended ? "YES" : "NO",
+            student.marks,
+          ];
+          Object.values(student.answers).forEach((answer) => {
+            tempCsvArray.push(answer.answer);
+          });
+          tempCsvData.push(tempCsvArray);
+        });
+        console.log(tempCsvData);
+        setCsvData(tempCsvData);
+        setStudents(tempStuds);
+      });
+    });
+  }, []);
   return (
     <>
-      <h1>Students Section</h1>
+      {students.length > 0 ? (
+        <>
+          <table>
+            <tbody>
+              <tr>
+                <th>Sl. No.</th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Attendance</th>
+                <th>Marks</th>
+              </tr>
+              {students.map((student, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{index + 1}</td>
+                    <td>{student.username}</td>
+                    <td>{student.email}</td>
+                    <td style={{ color: student.attended ? "green" : "red" }}>
+                      {student.attended ? "YES" : "NO"}
+                    </td>
+                    <td>{student.marks}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <div className="downloadBtns">
+            <button className="downloadBtn">Download Question Paper</button>
+            <button className="downloadBtn">
+              <CSVLink className="downloadBtnText" data={csvData}>
+                Download Student Answer
+              </CSVLink>
+            </button>
+          </div>
+        </>
+      ) : null}
     </>
   );
 }
